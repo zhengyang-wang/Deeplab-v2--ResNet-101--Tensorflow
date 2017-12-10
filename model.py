fb2d0069feff02540ef6e18ca58dd22d06c69b92
John1231983 <john1231983@gmail.com>
2017-12-10 17:22:14 +0900

    Add gradient accumulate for small batch size

--- model.py
+++ model.py
@@ -8,8 +8,6 @@ import tensorflow as tf
 from network import *
 from utils import ImageReader, decode_labels, inv_preprocess, prepare_label, write_log
 
-
-
 """
 This script trains or evaluates the model on augmented PASCAL VOC 2012 dataset.
 The training set contains 10581 training images.
@@ -20,12 +18,10 @@ Training:
 different learning rates for different layers
 """
 
+IMG_MEAN = np.array((104.00698793, 116.66876762, 122.67891434), dtype=np.float32)
 
-
-IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
 
 class Model(object):
-
 	def __init__(self, sess, conf):
 		self.sess = sess
 		self.conf = conf
@@ -44,24 +40,43 @@ class Model(object):
 		threads = tf.train.start_queue_runners(coord=self.coord, sess=self.sess)
 
 		# Train!
-		for step in range(self.conf.num_steps+1):
+		for step in range(self.conf.num_steps + 1):
 			start_time = time.time()
-			feed_dict = { self.curr_step : step }
-
-			if step % self.conf.save_interval == 0:
-				loss_value, images, labels, preds, summary, _ = self.sess.run(
-					[self.reduced_loss,
-					self.image_batch,
-					self.label_batch,
-					self.pred,
-					self.total_summary,
-					self.train_op],
-					feed_dict=feed_dict)
-				self.summary_writer.add_summary(summary, step)
-				self.save(self.saver, step)
+			feed_dict = {self.curr_step: step}
+			if self.conf.grad_update_every == 1:
+				if step % self.conf.save_interval == 0:
+					loss_value, images, labels, preds, summary, _ = self.sess.run(
+						[self.reduced_loss,
+						 self.image_batch,
+						 self.label_batch,
+						 self.pred,
+						 self.total_summary,
+						 self.train_op],
+						feed_dict=feed_dict)
+					self.summary_writer.add_summary(summary, step)
+					self.save(self.saver, step)
+				else:
+					loss_value, _ = self.sess.run([self.reduced_loss, self.train_op],
+												  feed_dict=feed_dict)
 			else:
-				loss_value, _ = self.sess.run([self.reduced_loss, self.train_op],
-					feed_dict=feed_dict)
+				loss_value = 0
+				# Clear the accumulated gradients.
+				self.sess.run(self.zero_op, feed_dict=feed_dict)
+
+				# Accumulate gradients.
+				for i in range(self.conf.grad_update_every):
+					_, l_val = self.sess.run([self.accum_grads_op, self.reduced_loss], feed_dict=feed_dict)
+					loss_value += l_val
+				# Normalise the loss.
+				loss_value /= self.conf.grad_update_every
+				# Apply gradients.
+				if step % self.conf.save_interval == 0:
+					images, labels, summary, _ = self.sess.run(
+						[self.image_batch, self.label_batch, self.total_summary, self.train_op], feed_dict=feed_dict)
+					self.summary_writer.add_summary(summary, step)
+					self.save(self.saver, step)
+				else:
+					self.sess.run(self.train_op, feed_dict=feed_dict)
 
 			duration = time.time() - start_time
 			print('step {:d} \t loss = {:.3f}, ({:.3f} sec/step)'.format(step, loss_value, duration))
@@ -71,6 +86,7 @@ class Model(object):
 		self.coord.request_stop()
 		self.coord.join(threads)
 
+
 	# evaluate
 	def test(self):
 		self.test_setup()
@@ -79,7 +95,7 @@ class Model(object):
 		self.sess.run(tf.local_variables_initializer())
 
 		# load checkpoint
-		checkpointfile = self.conf.modeldir+ '/model.ckpt-' + str(self.conf.valid_step)
+		checkpointfile = self.conf.modeldir + '/model.ckpt-' + str(self.conf.valid_step)
 		self.load(self.loader, checkpointfile)
 
 		# Start queue threads.
@@ -97,15 +113,16 @@ class Model(object):
 		self.coord.request_stop()
 		self.coord.join(threads)
 
+
 	def train_setup(self):
 		tf.set_random_seed(self.conf.random_seed)
-		
+
 		# Create queue coordinator.
 		self.coord = tf.train.Coordinator()
 
 		# Input size
 		input_size = (self.conf.input_height, self.conf.input_width)
-		
+
 		# Load reader
 		with tf.name_scope("create_inputs"):
 			reader = ImageReader(
@@ -118,7 +135,7 @@ class Model(object):
 				IMG_MEAN,
 				self.coord)
 			self.image_batch, self.label_batch = reader.dequeue(self.conf.batch_size)
-		
+
 		# Create network
 		if self.conf.encoder_name not in ['res101', 'res50', 'deeplab']:
 			print('encoder_name ERROR!')
@@ -131,7 +148,7 @@ class Model(object):
 			# Trainable Variables
 			all_trainable = tf.trainable_variables()
 			# Fine-tune part
-			encoder_trainable = [v for v in all_trainable if 'fc' not in v.name] # lr * 1.0
+			encoder_trainable = [v for v in all_trainable if 'fc' not in v.name]  # lr * 1.0
 			# Decoder part
 			decoder_trainable = [v for v in all_trainable if 'fc' in v.name]
 		else:
@@ -141,18 +158,18 @@ class Model(object):
 			# Trainable Variables
 			all_trainable = tf.trainable_variables()
 			# Fine-tune part
-			encoder_trainable = [v for v in all_trainable if 'resnet_v1' in v.name] # lr * 1.0
+			encoder_trainable = [v for v in all_trainable if 'resnet_v1' in v.name]  # lr * 1.0
 			# Decoder part
 			decoder_trainable = [v for v in all_trainable if 'decoder' in v.name]
-		
-		decoder_w_trainable = [v for v in decoder_trainable if 'weights' in v.name or 'gamma' in v.name] # lr * 10.0
-		decoder_b_trainable = [v for v in decoder_trainable if 'biases' in v.name or 'beta' in v.name] # lr * 20.0
+
+		decoder_w_trainable = [v for v in decoder_trainable if 'weights' in v.name or 'gamma' in v.name]  # lr * 10.0
+		decoder_b_trainable = [v for v in decoder_trainable if 'biases' in v.name or 'beta' in v.name]  # lr * 20.0
 		# Check
-		assert(len(all_trainable) == len(decoder_trainable) + len(encoder_trainable))
-		assert(len(decoder_trainable) == len(decoder_w_trainable) + len(decoder_b_trainable))
+		assert (len(all_trainable) == len(decoder_trainable) + len(encoder_trainable))
+		assert (len(decoder_trainable) == len(decoder_w_trainable) + len(decoder_b_trainable))
 
 		# Network raw output
-		raw_output = net.outputs # [batch_size, h, w, 21]
+		raw_output = net.outputs  # [batch_size, h, w, 21]
 
 		# Output size
 		output_shape = tf.shape(raw_output)
@@ -160,7 +177,7 @@ class Model(object):
 
 		# Groud Truth: ignoring all labels greater or equal than n_classes
 		label_proc = prepare_label(self.label_batch, output_size, num_classes=self.conf.num_classes, one_hot=False)
-		raw_gt = tf.reshape(label_proc, [-1,])
+		raw_gt = tf.reshape(label_proc, [-1, ])
 		indices = tf.squeeze(tf.where(tf.less_equal(raw_gt, self.conf.num_classes - 1)), 1)
 		gt = tf.cast(tf.gather(raw_gt, indices), tf.int32)
 		raw_prediction = tf.reshape(raw_output, [-1, self.conf.num_classes])
@@ -187,16 +204,34 @@ class Model(object):
 		# To make sure each layer gets updated by different lr's, we do not use 'minimize' here.
 		# Instead, we separate the steps compute_grads+update_params.
 		# Compute grads
-		grads = tf.gradients(self.reduced_loss, encoder_trainable + decoder_w_trainable + decoder_b_trainable)
-		grads_encoder = grads[:len(encoder_trainable)]
-		grads_decoder_w = grads[len(encoder_trainable) : (len(encoder_trainable) + len(decoder_w_trainable))]
-		grads_decoder_b = grads[(len(encoder_trainable) + len(decoder_w_trainable)):]
+		if self.conf.grad_update_every == 1:
+			grads = tf.gradients(self.reduced_loss, encoder_trainable + decoder_w_trainable + decoder_b_trainable)
+			grads_encoder = grads[:len(encoder_trainable)]
+			grads_decoder_w = grads[len(encoder_trainable): (len(encoder_trainable) + len(decoder_w_trainable))]
+			grads_decoder_b = grads[(len(encoder_trainable) + len(decoder_w_trainable)):]
+		else:
+			accum_grads = [tf.Variable(tf.zeros_like(v.initialized_value()),
+									   trainable=False) for v in
+						   encoder_trainable + decoder_w_trainable + decoder_b_trainable]
+			# Define an operation to clear the accumulated gradients for next batch.
+			self.zero_op = [v.assign(tf.zeros_like(v)) for v in accum_grads]
+			# Compute gradients.
+			grads = tf.gradients(self.reduced_loss, encoder_trainable + decoder_w_trainable + decoder_b_trainable)
+			# Accumulate and normalise the gradients.
+			self.accum_grads_op = [accum_grads[i].assign_add(grad / self.conf.grad_update_every) for i, grad in
+								   enumerate(grads)]
+			grads_encoder = accum_grads[:len(encoder_trainable)]
+			grads_decoder_w = accum_grads[len(encoder_trainable): (len(encoder_trainable) + len(decoder_w_trainable))]
+			grads_decoder_b = accum_grads[(len(encoder_trainable) + len(decoder_w_trainable)):]
+
+
 		# Update params
 		train_op_conv = opt_encoder.apply_gradients(zip(grads_encoder, encoder_trainable))
 		train_op_fc_w = opt_decoder_w.apply_gradients(zip(grads_decoder_w, decoder_w_trainable))
 		train_op_fc_b = opt_decoder_b.apply_gradients(zip(grads_decoder_b, decoder_b_trainable))
+
 		# Finally, get the train_op!
-		update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS) # for collecting moving_mean and moving_variance
+		update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)  # for collecting moving_mean and moving_variance
 		with tf.control_dependencies(update_ops):
 			self.train_op = tf.group(train_op_conv, train_op_fc_w, train_op_fc_b)
 
@@ -216,11 +251,12 @@ class Model(object):
 		labels_summary = tf.py_func(decode_labels, [self.label_batch, 2, self.conf.num_classes], tf.uint8)
 		preds_summary = tf.py_func(decode_labels, [self.pred, 2, self.conf.num_classes], tf.uint8)
 		self.total_summary = tf.summary.image('images',
-			tf.concat(axis=2, values=[images_summary, labels_summary, preds_summary]),
-			max_outputs=2) # Concatenate row-wise.
+											  tf.concat(axis=2, values=[images_summary, labels_summary, preds_summary]),
+											  max_outputs=2)  # Concatenate row-wise.
 		if not os.path.exists(self.conf.logdir):
 			os.makedirs(self.conf.logdir)
 		self.summary_writer = tf.summary.FileWriter(self.conf.logdir, graph=tf.get_default_graph())
+
 
 	def test_setup(self):
 		# Create queue coordinator.
@@ -231,16 +267,16 @@ class Model(object):
 			reader = ImageReader(
 				self.conf.data_dir,
 				self.conf.valid_data_list,
-				None, # the images have different sizes
-				False, # no data-aug
-				False, # no data-aug
+				None,  # the images have different sizes
+				False,  # no data-aug
+				False,  # no data-aug
 				self.conf.ignore_label,
 				IMG_MEAN,
 				self.coord)
-			image, label = reader.image, reader.label # [h, w, 3 or 1]
+			image, label = reader.image, reader.label  # [h, w, 3 or 1]
 		# Add one batch dimension [1, h, w, 3 or 1]
 		self.image_batch, self.label_batch = tf.expand_dims(image, dim=0), tf.expand_dims(label, dim=0)
-		
+
 		# Create network
 		if self.conf.encoder_name not in ['res101', 'res50', 'deeplab']:
 			print('encoder_name ERROR!')
@@ -253,12 +289,12 @@ class Model(object):
 
 		# predictions
 		raw_output = net.outputs
-		raw_output = tf.image.resize_bilinear(raw_output, tf.shape(self.image_batch)[1:3,])
+		raw_output = tf.image.resize_bilinear(raw_output, tf.shape(self.image_batch)[1:3, ])
 		raw_output = tf.argmax(raw_output, axis=3)
 		pred = tf.expand_dims(raw_output, dim=3)
-		self.pred = tf.reshape(pred, [-1,])
+		self.pred = tf.reshape(pred, [-1, ])
 		# labels
-		gt = tf.reshape(self.label_batch, [-1,])
+		gt = tf.reshape(self.label_batch, [-1, ])
 		# Ignoring all labels greater than or equal to n_classes.
 		temp = tf.less_equal(gt, self.conf.num_classes - 1)
 		weights = tf.cast(temp, tf.int32)
@@ -277,6 +313,7 @@ class Model(object):
 		# Loader for loading the checkpoint
 		self.loader = tf.train.Saver(var_list=tf.global_variables())
 
+
 	def save(self, saver, step):
 		'''
 		Save weights.
@@ -288,9 +325,10 @@ class Model(object):
 		saver.save(self.sess, checkpoint_path, global_step=step)
 		print('The checkpoint has been created.')
 
+
 	def load(self, saver, filename):
 		'''
 		Load trained weights.
-		''' 
+		'''
 		saver.restore(self.sess, filename)
 		print("Restored model parameters from {}".format(filename))
